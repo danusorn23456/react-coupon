1a89638fb4c86f03ef1acf6caa7a0431d5c97bf4
danusorn23456 <danusorn23456@gmail.com>
2023-06-06 06:47:45 +0700

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import { Coupon, CouponProps } from "./components";
+
+function getDatePlusTenDays() {
+  var currentDate = new Date(); // Get the current date
+  currentDate.setDate(currentDate.getDate() + 9); // Add 10 days to the current date
+
+  // Extract the year, month, and day from the date
+  var year = currentDate.getFullYear();
+  var month = ("0" + (currentDate.getMonth() + 1)).slice(-2); // Months are zero-based
+  var day = ("0" + currentDate.getDate()).slice(-2);
+
+  var formattedDate = year + "-" + month + "-" + day; // Format the date as "YYYY-MM-DD"
+
+  return new Date(formattedDate);
+}
+
+const coupons: CouponProps[] = [
+  {
+    title: "Save 50%",
+    description: "minimium 3 orders",
+    image:
+      "https://static.vecteezy.com/system/resources/previews/011/016/165/original/realistic-3d-gift-box-cutout-free-png.png",
+    expireDate: getDatePlusTenDays(),
+    backgroundColor: "#ffffff",
+    usedDate: new Date(),
+    tag: ["new account"],
+  },
+  {
+    title: "Save 20 THB",
+    description: "minimium 500 THB",
+    image:
+      "https://static.vecteezy.com/system/resources/previews/008/854/705/original/3d-coin-for-finance-or-business-illustration-png.png",
+    expireDate: getDatePlusTenDays(),
+    usedDate: new Date(),
+  },
+  {
+    title: "Free Shipping",
+    description: "minimium 1200 THB",
+    image:
+      "https://cdn3d.iconscout.com/3d/premium/thumb/truck-6847901-5606997.png",
+    expireDate: getDatePlusTenDays(),
+    usedDate: "",
+  },
+];
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          padding: 8,
+          width: "400px",
+        }}
+      >
+        {coupons.map((coupon, index) => (
+          <div
+            style={{
+              marginBottom: 12,
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Coupon key={index} {...coupon} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/coupon/index.tsx
@@ -0,0 +1,84 @@
+import React, { CSSProperties, HTMLAttributes } from "react";
+import "./index.css";
+
+export interface CouponProps extends HTMLAttributes<HTMLDivElement> {
+  title: string;
+  description: string;
+  backgroundColor?: string;
+  fontColor?: string;
+  image?: string;
+  expireDate?: Date;
+  usedDate?: Date | "";
+  usedLabel?: (date: Date) => any;
+  expireLabel?: (days: number) => any;
+  tag?: string[];
+}
+
+function Coupon({
+  title,
+  description,
+  backgroundColor = "#FFFFFF",
+  fontColor = "#393939",
+  usedLabel = (date: Date) => `used by ${date.toDateString()}`,
+  expireLabel = (days: number) =>
+    days ? `Valid for ${days} days from now` : `expire`,
+  expireDate,
+  usedDate,
+  image,
+  tag,
+  ...rest
+}: CouponProps) {
+  function getRemainingDays(couponExpirationDate?: Date) {
+    if (!couponExpirationDate) {
+      return 0;
+    }
+
+    var currentDate = new Date(); // Get the current date
+
+    // Convert the coupon expiration date string to a Date object
+    var expirationDate = new Date(couponExpirationDate);
+
+    // Calculate the time difference in milliseconds between the expiration date and the current date
+    var timeDifference = expirationDate.getTime() - currentDate.getTime();
+
+    // Convert the time difference to days
+    var remainingDays = Math.ceil(timeDifference / (1000 * 3600 * 24));
+
+    return +remainingDays as number;
+  }
+
+  const remaining = getRemainingDays(expireDate);
+
+  return (
+    <div
+      {...rest}
+      className="r-coupon"
+      style={
+        { "--r-coupon-background-color": backgroundColor } as CSSProperties
+      }
+    >
+      <div className="r-coupon-media">
+        <img className="r-coupon-image" src={image} alt={`coupon-${title}`} />
+      </div>
+      <div className="r-coupon-dashed"></div>
+      <div className="r-coupon-content">
+        <div className="r-coupon-header" style={{ color: fontColor }}>
+          <h3 className="r-coupon-title">{title}</h3>
+          <p className="r-coupon-description">{description}</p>
+        </div>
+        <div className="r-coupon-tags" style={{ color: fontColor }}>
+          {tag?.map((tag, index) => (
+            <p className="r-coupon-tag" key={index}>
+              {tag}
+            </p>
+          ))}
+        </div>
+        <p className="r-coupon-date" style={{ color: fontColor }}>
+          {usedDate ? usedLabel(usedDate) : expireLabel(remaining)}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export { Coupon };
